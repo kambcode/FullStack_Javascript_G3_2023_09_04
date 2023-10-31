61cfe60bd9279f0f2d66195b6a247009d6637cc9
Camilo Montoya <kamilo2428@gmail.com>
2023-10-30 20:58:06 -0500

    clase 4

--- MODULO_2/JS_C4/workclass/loops.js
+++ MODULO_2/JS_C4/workclass/loops.js
@@ -3,11 +3,12 @@ let numero = 1;
 
 while (numero <= 10) { // Mientras que la condición sea verdadera se va a ejecutar el codigo de adentro
     console.log('Numero', numero);
-    numero = numero + 1; // Aca numero vale 2
+    // numero = numero + 1; // Tambien se puede escribir así  numero++;
+    numero++; //Tambien se puede escribir así numero = numero + 1;
 }
 console.log('FIN')
 
-// if (numero <= 10) {
-//     // solamente si condition es true (verdadero)
-//     console.log(numero)
-// }
+
+// // IMPORTANTE
+// numero++; // numero = numero + 1;
+// numero--; // numero = numero - 1;
---
+++ MODULO_2/JS_C4/workclass/loops_v2.js
@@ -0,0 +1,14 @@
+// Queremos hacer un programa que muestre los numeros del 1 a 10
+
+console.log('EJERCICIO USANDO FOR')
+
+for (let numero = 1; numero <= 10; numero++) {
+   // Simepre que numero <= 10
+   // este codigo se va a ejecutar
+   console.log('Numero', numero);
+}
+
+
+// numero++; // se puede escribir asi: numero = numero + 1;
+
+console.log('FIN')
