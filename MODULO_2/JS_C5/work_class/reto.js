9651ba0d3605f7d25b3c471163e8d212a70edf26
Camilo Montoya <kamilo2428@gmail.com>
2023-11-03 21:13:37 -0500

    class 5

---
+++ MODULO_2/JS_C5/work_class/reto.js
@@ -0,0 +1,18 @@
+const myArray = [];
+// Llenar el arreglo
+for (let index = 0; index <= 100; index++) {
+    myArray.push(index)
+}
+console.log(myArray); // debe imprime un array con los numeros de 1 al 100
+
+// Parte 2, Obtener la suma de todos los valores de el array.
+// Utilizando un for / while, vamos a recorrer el arreglo myArray
+// para ir sumando los valores que contiene el arreglo
+
+let suma = 0;
+// Recorrer los elementos que tiene myArray
+// ir sumando en la variable suma el valor de cada elemento
+
+// suma = suma + <CUALQUIER_NUMERO>
+
+console.log(suma); // 5050
