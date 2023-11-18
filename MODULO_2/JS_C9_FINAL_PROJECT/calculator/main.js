0aba34fd2e14429f49ee64364d111d29f8d68bb4
Camilo Montoya <kamilo2428@gmail.com>
2023-11-17 20:32:15 -0500

    projecto final

--- MODULO_2/JS_C9_FINAL_PROJECT/calculator/main.js
+++ MODULO_2/JS_C9_FINAL_PROJECT/calculator/main.js
@@ -24,3 +24,25 @@ const result = () => {
     resultado = resultado + screenNumber;
     screenContainer.innerHTML = resultado;
 }
+
+
+// Explicacion extra, esto no tiene nada que ver con la calculadora
+
+// const baseDeDatos = ["camilo", "robin", "alejandra"]
+// const baseDeDatosTel = [32434234, 424234234, 45656456]
+
+// let base = [
+//     { name: "camilo", tel: 24324234},
+//     { name: "robin", tel: 3333333},
+//     { name: "alejandra", tel: 77777777}
+// ]
+
+// console.log(base[0]) //  { name: "camilo", tel: 24324234}
+// console.log(base[1]) //  { name: "robin", tel: 3333333}
+// console.log(base[2]) //  { name: "alejandra", tel: 77777777}
+
+
+// function register () {
+//     // 
+//     base.push({ name: "Gullermo", tel: 646464646})
+// }
